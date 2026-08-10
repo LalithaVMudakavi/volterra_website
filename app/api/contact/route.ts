8d90337a4d0ba40247dcc8d7c7b7b995f782d68f
LalithaVMudakavi <130538913+LalithaVMudakavi@users.noreply.github.com>
2026-08-10 18:58:15 +0530

    Initial commit

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+import { Analytics } from "@vercel/analytics/next"
 
 export async function POST(request: Request) {
   try {
